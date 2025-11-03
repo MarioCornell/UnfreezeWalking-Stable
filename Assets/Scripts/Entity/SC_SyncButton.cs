05465f6add32fd8cf9b4b10bb0bd1b29685d3449
MarioCornell <cy546@cornell.edu>
2025-11-02 21:20:06 -0500

    update

--- Assets/Scripts/Entity/SC_SyncButton.cs
+++ Assets/Scripts/Entity/SC_SyncButton.cs
@@ -8,16 +8,12 @@ using UnityEngine.Events;
 
 public class SC_SyncButton : NetworkBehaviour
 {
-    [SerializeField] private float threshold = 0.2f;
     [SerializeField] private float debounceTime = 1f;
+    [SerializeField] private Animator animator;
 
     private bool _isPressed = false;
-    private Vector3 _startPosition;
-    private ConfigurableJoint _joint;
-    private Transform _movingPart;
-    private float _timeOfLastEvent;
 
-    public UnityEvent onPressed, onReleased;
+    public UnityEvent onPressed;
 
     public override void OnNetworkSpawn()
     {
@@ -29,52 +25,28 @@ public class SC_SyncButton : NetworkBehaviour
 
     private void Start()
     {
-        _joint = GetComponentInChildren<ConfigurableJoint>();
-        _movingPart = _joint.transform;
-        _startPosition = _movingPart.localPosition;
-        _timeOfLastEvent = -debounceTime;
+
     }
 
-    private void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        if (Time.time < _timeOfLastEvent + debounceTime)
-        {
-            return;
-        }
-
-        if (!_isPressed && GetValue() + threshold >= 1)
-        {
-            Pressed();
-        }
-        else if (_isPressed && GetValue() - threshold <= 0)
-        {
-            Released();
-        }
-    }
-
-    private float GetValue()
-    {
-        var value = Vector3.Distance(_startPosition, _movingPart.localPosition) / _joint.linearLimit.limit;
-        return Mathf.Clamp01(value);
+        Pressed();
+        
+        // start the "buttonpress" animation, call it directly
     }
 
     private void Pressed()
     {
         _isPressed = true;
-        _timeOfLastEvent = Time.time;
         onPressed.Invoke();
         Debug.Log("Sync Button Pressed");
 
         FindObjectOfType<SC_RecButton>().ToggleLogging();
-
-        
     }
 
     private void Released()
     {
         _isPressed = false;
-        _timeOfLastEvent = Time.time;
-        onReleased.Invoke();
         Debug.Log("Sync Button Released");
     }
 }
